00a4e8f55c67888ba530221d4f5736a8254f6dd5
wyc <3066171866@qq.com>
2025-09-30 12:47:36 +0800

    实现水印拖拽功能以及保存水印模块和加载水印模块功能

--- Photo_Watermark_2/src/main/java/com/puxinheihei/entity/WatermarkConfig.java
+++ Photo_Watermark_2/src/main/java/com/puxinheihei/entity/WatermarkConfig.java
@@ -2,6 +2,7 @@ package com.puxinheihei.entity;
 
 import lombok.Data;
 import javafx.scene.paint.Color;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 public class WatermarkConfig {
@@ -9,7 +10,13 @@ public class WatermarkConfig {
     private String text;
     private String fontFamily;
     private double fontSize;
+
+    @JsonIgnore  // 忽略JavaFX Color的序列化
     private Color textColor;
+
+    // 添加用于JSON序列化的颜色字段
+    private String textColorHex;
+
     private double textOpacity;
 
     // 位置配置
@@ -30,7 +37,26 @@ public class WatermarkConfig {
         this.fontFamily = "Microsoft YaHei";
         this.fontSize = 48;
         this.textColor = Color.BLACK;
+        this.textColorHex = "#000000";
         this.textOpacity = 0.7;
         this.position = Position.BOTTOM_RIGHT;
+    }
+
+    // Color和Hex颜色转换方法
+    public void setTextColor(Color color) {
+        this.textColor = color;
+        if (color != null) {
+            this.textColorHex = String.format("#%02X%02X%02X",
+                    (int) (color.getRed() * 255),
+                    (int) (color.getGreen() * 255),
+                    (int) (color.getBlue() * 255));
+        }
+    }
+
+    public Color getTextColor() {
+        if (textColorHex != null && !textColorHex.isEmpty()) {
+            return Color.web(textColorHex);
+        }
+        return textColor;
     }
 }
